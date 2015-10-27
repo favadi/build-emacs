0e7dcba019a441e29e8a2faa117ef494aae52be7
Diep Pham <favadi@fastmail.fm>
2015-10-27 23:45:56 +0700

    remove libghc-gconf-dev

--- build-emacs.sh
+++ build-emacs.sh
@@ -14,7 +14,7 @@ sudo apt-get -qq update
 sudo apt-get -qq install -y stow build-essential libx11-dev xaw3dg-dev \
      libjpeg-dev libpng12-dev libgif-dev libtiff5-dev libncurses5-dev \
      libxft-dev librsvg2-dev libmagickcore-dev libmagick++-dev \
-     libxml2-dev libgpm-dev libghc-gconf-dev libotf-dev libm17n-dev \
+     libxml2-dev libgpm-dev libotf-dev libm17n-dev \
      libgnutls-dev wget
 
 # download source package
