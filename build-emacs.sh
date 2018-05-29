4a2df0b53512e2882beee7192ab6b0464279de61
Diep Pham <me@favadi.com>
2018-05-29 23:50:22 +0700

    build on OpenSUSE

--- build-emacs.sh
+++ build-emacs.sh
@@ -6,19 +6,10 @@
 
 set -eu
 
-readonly version="25.3"
+readonly version="26.1"
 
 # install dependencies
-sudo apt-get -qq update
-sudo apt-get -qq install -y stow build-essential libx11-dev \
-     libjpeg-dev libgif-dev libtiff5-dev libncurses5-dev \
-     libxft-dev librsvg2-dev libmagickcore-dev libmagick++-dev \
-     libxml2-dev libgpm-dev libotf-dev libm17n-dev \
-     libgtk-3-dev libwebkitgtk-3.0-dev libxpm-dev wget
-
-# from Ubuntu 16.10, libgnutls-dev, libpng12-dev is no longer available
-sudo apt-get -qq install libgnutls-dev libpng12-dev || \
-    sudo apt-get -qq install libgnutls28-dev libpng-dev
+sudo zypper install --no-confirm stow xorg-x11-devel gtk3-devel libgnutls-devel
 
 # download source package
 if [[ ! -d emacs-"$version" ]]; then
@@ -34,7 +25,10 @@ sudo mkdir -p /usr/local/stow
 cd emacs-"$version"
 ./configure \
     --with-xft \
-    --with-x-toolkit=gtk3
+    --with-x-toolkit=gtk3 \
+    --with-jpeg=no \
+    --with-gif=no \
+    --with-tiff=no
 
 make
 sudo make \
